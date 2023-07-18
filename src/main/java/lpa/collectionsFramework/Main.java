aa6beead70c6917477afe3d4a5b674d7825555d7
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-18 16:50:51 +0200

    Examples of use of the copy, fill, addAll and nCopies methods from Collections.

--- src/main/java/lpa/collectionsFramework/Main.java
+++ src/main/java/lpa/collectionsFramework/Main.java
@@ -1,4 +1,4 @@
-package lpa.dev;
+package lpa.collectionsFramework;
 
 import java.util.Arrays;
 import java.util.Collection;
--- src/main/java/lpa/deckOfCards/Main.java
+++ src/main/java/lpa/deckOfCards/Main.java
@@ -1,6 +1,6 @@
 package lpa.deckOfCards;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -14,5 +14,55 @@ public class Main {
 //        2♦(0) 3♦(1) 4♦(2) 5♦(3) 6♦(4) 7♦(5) 8♦(6) 9♦(7) 10♦(8) J♦(9) Q♦(10) K♦(11) A♦(12)
 //        2♥(0) 3♥(1) 4♥(2) 5♥(3) 6♥(4) 7♥(5) 8♥(6) 9♥(7) 10♥(8) J♥(9) Q♥(10) K♥(11) A♥(12)
 //        2♠(0) 3♠(1) 4♠(2) 5♠(3) 6♠(4) 7♠(5) 8♠(6) 9♠(7) 10♠(8) J♠(9) Q♠(10) K♠(11) A♠(12)
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+//        --------------------
+//        Aces of Hearts
+//        A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12)
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+//        []
+//        cards.size() = 0
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+//        --------------------
+//        Aces of Hearts
+//        A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12)
+
+        Card kingOfClubs = Card.getFaceCard(Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+//        --------------------
+//        Kings of Clubs
+//        K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11)
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card collection with Aces added", 2);
+//        --------------------
+//        Card collection with Aces added
+//        A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12)
+//        A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12)
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card collection with Kings copied.", 2);
+//        --------------------
+//        Card collection with Kings copied.
+//        K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11)
+//        A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12) A♥(12)
+
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List copy of Kings", 1);
+//        --------------------
+//        Card collection with Kings copied
+//        K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11)
+//        K♣(11) K♣(11) K♣(11) K♣(11) K♣(11) K♣(11)
     }
 }
