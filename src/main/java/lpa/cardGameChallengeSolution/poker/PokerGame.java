3b78670ed57464a0e58968cafa245d90baa32c59
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-21 17:38:17 +0200

    Evaluation methods for players hand

--- src/main/java/lpa/cardGameChallengeSolution/poker/PokerGame.java
+++ src/main/java/lpa/cardGameChallengeSolution/poker/PokerGame.java
@@ -3,6 +3,7 @@ package lpa.cardGameChallengeSolution.poker;
 import lpa.deckOfCards.Card;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class PokerGame {
 
@@ -27,7 +28,15 @@ public class PokerGame {
 
         deal();
         System.out.println("-".repeat(20));
-        pokerHands.forEach(System.out::println);
+        Consumer<PokerHand> checkHand = PokerHand::evalHand;
+        pokerHands.forEach(checkHand.andThen(System.out::println));
+
+        int cardsDealt = playerCount * cardsInHand;
+        int cardsRemaining = deck.size() - cardsDealt;
+
+        remainingCards = new ArrayList<>(Collections.nCopies(cardsRemaining, null));
+        remainingCards.replaceAll(c -> deck.get(cardsDealt + remainingCards.indexOf(c)));
+        Card.printDeck(remainingCards, "Remaining Cards", 2);
     }
 
     private void deal() {
--- src/main/java/lpa/cardGameChallengeSolution/poker/PokerHand.java
+++ src/main/java/lpa/cardGameChallengeSolution/poker/PokerHand.java
@@ -2,8 +2,7 @@ package lpa.cardGameChallengeSolution.poker;
 
 import lpa.deckOfCards.Card;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PokerHand {
 
@@ -23,7 +22,60 @@ public class PokerHand {
 
     @Override
     public String toString() {
-        return "%d. %-16s Rank:%d %-40s %s".formatted(
-                playerNo, score, score.ordinal(), hand, (discards.size() > 0) ? "Discards:" + discards : "");
+        return "%d. %-16s Rank:%d %-40s Best:%-7s Worst:%-6s %s".formatted(
+                playerNo, score, score.ordinal(), hand, Collections.max(hand, Comparator.comparing(Card::rank)),
+                Collections.min(hand, Comparator.comparing(Card::rank)),
+                (discards.size() > 0) ? "Discards:" + discards : "");
+    }
+
+    private void setRank(int faceCount) {
+
+        switch (faceCount) {
+            case 4 -> score = Ranking.FOUR_OF_A_KIND;
+            case 3 -> {
+                if (score == Ranking.NONE) score = Ranking.THREE_OF_A_KIND;
+                else score = Ranking.FULL_HOUSE;
+            }
+            case 2 -> {
+                if (score == Ranking.NONE) score = Ranking.ONE_PAIR;
+                else if (score == Ranking.THREE_OF_A_KIND) score = Ranking.FULL_HOUSE;
+                else score = Ranking.TWO_PAIR;
+            }
+        }
+    }
+
+    public void evalHand() {
+        List<String> faceList = new ArrayList<>(hand.size());
+        hand.forEach(card -> faceList.add(card.face()));
+
+        List<String> duplicateFaceCards = new ArrayList<>();
+        faceList.forEach(face -> {
+            if (!duplicateFaceCards.contains(face) && Collections.frequency(faceList, face) > 1) {
+                duplicateFaceCards.add(face);
+            }
+        });
+
+        for (String duplicate : duplicateFaceCards) {
+            int start = faceList.indexOf(duplicate);
+            int last = faceList.lastIndexOf(duplicate);
+            setRank(last - start + 1);
+            List<Card> sub = hand.subList(start, last + 1);
+            keepers.addAll(sub);
+        }
+
+        pickDiscards();
+    }
+
+    private void pickDiscards() {
+
+        List<Card> temp = new ArrayList<>(hand);
+        temp.removeAll(keepers);
+        int rankedCards = keepers.size();
+        Collections.reverse(temp);
+        int index = 0;
+        for (Card c : temp) {
+            if (index++ < 3 && (rankedCards > 2 || c.rank() < 9)) discards.add(c);
+            else keepers.add(c);
+        }
     }
 }
